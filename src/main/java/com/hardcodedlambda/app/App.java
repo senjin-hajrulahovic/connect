be2932440ae102cb2e49d34866ac37647e08d62f
senjin.hajrulahovic <senjin.hajrulahovic@gmail.com>
2018-10-18 00:03:00 +0200

    Configure maven plugins

--- src/main/java/com/hardcodedlambda/app/App.java
+++ src/main/java/com/hardcodedlambda/app/App.java
@@ -1,7 +1,16 @@
 package com.hardcodedlambda.app;
 
+import org.apache.commons.cli.*;
+
 public class App {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws ParseException {
+
+        Options options = new Options();
+        options.addOption("c", true, "catcher");
+
+        CommandLineParser cliParser = new DefaultParser();
+        CommandLine cmd = cliParser.parse(options, args);
+
+        System.out.println("Hello cmd!");
     }
 }
